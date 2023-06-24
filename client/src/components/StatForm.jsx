6a1138fc6f584cb5a13b462d90469cbd0edc92b6
Billy Franklin <120335506+BillyFranklinNola@users.noreply.github.com>
2023-06-24 18:20:24 -0500

    correct all front end routes to match backend

--- client/src/components/StatForm.jsx
+++ client/src/components/StatForm.jsx
@@ -30,7 +30,7 @@ const StatForm = (props) => {
             <form className='mx-auto' onSubmit={onSubmitHandler}>
                 <div className='form-group m-3'>
                     <label htmlFor='minutes'>Minutes</label>
-                    <input type="minutes" name="minutes" id="minutes" className="form-control" value={gameStats.minutes} onChange = {changeHandler}/>
+                    <input type="minutes" name="minutes" id="minutes" className="form-control" onChange = {changeHandler}/>
                     {
                         errors.minutes?
                         <p>{errors.minutes.message}</p>:
@@ -39,7 +39,7 @@ const StatForm = (props) => {
                 </div>
                 <div className='form-group m-3'>
                     <label htmlFor='address'>Goals</label>
-                    <input type="goals" name="goals" id="goals" className="form-control" value={gameStats.goals} onChange = {changeHandler}/>
+                    <input type="goals" name="goals" id="goals" className="form-control" onChange = {changeHandler}/>
                     {
                         errors.goals?
                         <p>{errors.goals.message}</p>:
@@ -48,7 +48,7 @@ const StatForm = (props) => {
                 </div>
                 <div className='form-group m-3'>
                     <label htmlFor='assists'>Assists</label>
-                    <input type="text" name="assists" id="assists" className="form-control" value={gameStats.assists} onChange = {changeHandler}/>
+                    <input type="text" name="assists" id="assists" className="form-control" onChange = {changeHandler}/>
                     {
                         errors.assists?
                         <p>{errors.assists.message}</p>:
@@ -68,7 +68,8 @@ const StatForm = (props) => {
                     <label htmlFor='yes'>Yes</label>
                     <input type="radio" className="form-radio-input" name="RedCard" id="yes" onChange = {changeHandler}/>
                     <label htmlFor='no'>No</label>
-                    <input type="radio" className="form-radio-input" name="RedCard" id="no" onChange = {changeHandler}/>                    {
+                    <input type="radio" className="form-radio-input" name="RedCard" id="no" onChange = {changeHandler}/>
+                    {
                         errors.redCard?
                         <p>{errors.redCard.message}</p>:
                         null
