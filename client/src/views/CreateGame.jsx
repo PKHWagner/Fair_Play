41cfb0dfd015255147f9d7bab743a19c4873da0e
PKHWagner <se.aksnow@icloud.com>
2023-06-28 21:16:51 -0800

    small bug fix

--- client/src/views/CreateGame.jsx
+++ client/src/views/CreateGame.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import GameForm from '../components/GameForm'
 import PlayerNavbar from '../components/PlayerNavbar'
 import { useSelector } from 'react-redux';
@@ -15,33 +15,33 @@ const CreateGame = (props) => {
     const loggedInPlayer = useSelector((state) => state.auth.player);
 
     const newGame = async (game) => {
-        try{
+        try {
             const res = await axios.post('http://localhost:8000/api/games', game)
-                console.log(res);
-                setGame(res.data)
-                setAllGames([...allGames, res.data]);
-                navigate('/PlayerDashboardgit branch -a')
-            } catch (err) {
-                console.log(err.response.data.err.errors)
-                const errorResponse = err.response.data.err.errors;
-                const errorArray = [];
-                console.log(errorArray)
-                for (const key of Object.keys(errorResponse)) {
-                    {errorArray.push(errorResponse[key].message)}
-                    toast.error(errorResponse[key].message)
-                }
+            console.log(res);
+            setGame(res.data)
+            setAllGames([...allGames, res.data]);
+            navigate('/PlayerDashboard')
+        } catch (err) {
+            console.log(err.response.data.err.errors)
+            const errorResponse = err.response.data.err.errors;
+            const errorArray = [];
+            console.log(errorArray)
+            for (const key of Object.keys(errorResponse)) {
+                { errorArray.push(errorResponse[key].message) }
+                toast.error(errorResponse[key].message)
+            }
             setErrors(errorArray);
         }
     }
 
-return (
-    <div>
-        <PlayerNavbar player={loggedInPlayer.player}/>
-        <h2 className="mx-auto mt-5">Create Pick Up Game:</h2>
+    return (
         <div>
-            <GameForm onSubmitProp={newGame} initialGameDate="" initialAddress="" initialCity="" initialState=""  initialZipCode="" initialSetupTime="" initialKickOffTime=""/>
+            <PlayerNavbar player={loggedInPlayer.player} />
+            <h2 className="mx-auto mt-5">Create Pick Up Game:</h2>
+            <div>
+                <GameForm onSubmitProp={newGame} initialGameDate="" initialAddress="" initialCity="" initialState="" initialZipCode="" initialSetupTime="" initialKickOffTime="" />
+            </div>
         </div>
-    </div>
     )
 }
 
